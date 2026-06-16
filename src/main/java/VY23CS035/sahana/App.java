802464e0e6d52399ecaec5bcc92cf1846965ef06
sahanak05 <shreyakulk2@gmail.com>
2026-06-16 13:33:45 +0530

    initial commit

---
+++ src/main/java/VY23CS035/sahana/App.java
@@ -0,0 +1,13 @@
+package VY23CS035.sahana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
